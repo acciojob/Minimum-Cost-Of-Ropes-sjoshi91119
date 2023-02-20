84a9b42ec6b040cfad95423bc21f419fe625fe50
sjoshi91119 <115713904+sjoshi91119@users.noreply.github.com>
2023-02-20 10:25:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  unction minimumCost() {    
+    var res = 0;
+    var cost = 0;
+    var inputArray = document.getElementById("inputArr").value;
+    inputArray = inputArray.split(',').map(Number);
+    for (let i = 0; i < inputArray.length - 1; i += 0) {
+        if (inputArray.length == 0) break;
+		// sort by decreasing order
+        inputArray = inputArray.sort((a,b) => b-a);
+        res = inputArray.pop() + inputArray.pop();
+        cost += res;
+		// adding res to arrayy
+        inputArray[inputArray.length] = res;   
+    }  
+    document.getElementById("result").innerHTML = cost;
+}
 }  
