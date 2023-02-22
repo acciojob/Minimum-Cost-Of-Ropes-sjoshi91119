2389885f43273edcc0a6e2cccdc148ce10a51345
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-02-22 08:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-function calculateMinCost() {
-  //your code here
-function minCostToFormRope(event){
-	event.preventDefault();
-	var inputElement = document.querySelector('input').value;
-	var arr = inputElement.split(',');
-	arr.sort(function(a,b){return a-b});
-// we need to sort and remove 1 and 2 arr element
-	var cost =0;
-while(arr.length>1)
-	{
-		var res = Number(arr[0])+Number(arr[1]);
-		arr.push(res);
-		cost+=res;
-		
-		arr.shift();
-		arr.shift();
+function calculateMinCost() {  
+    var res = 0;  
+      var cost = 0;
+  var inputArray = document.getElementById("rope-lengths").value;
 
-arr.sort(function(a,b){return a-b});
-		
-	}
-document.getElementById("result").textContent=cost;
+  inputArray = inputArray.split(',').map(Number);
+  console.log(inputArray);
+  for (let i = 0; i < inputArray.length - 1; i += 0) {
+      if (inputArray.length == 0) break;
+      inputArray = inputArray.sort((a,b) => b-a);
+      res = inputArray.pop() + inputArray.pop();
+      cost += res;
+      inputArray[inputArray.length] = res; }
+  document.getElementById("result").innerHTML = cost;
 }
-}  
